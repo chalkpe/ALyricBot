dfad25f1c1a041a4d00f73a3a53371ed62374cc0
JellyBrick <shlee1503@naver.com>
2022-01-28 16:10:11 +0900

    bump deps (TelegramBots)

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,14 +14,8 @@ repositories {
 }
 
 dependencies {
-    implementation(group = "com.fasterxml.jackson.module", name = "jackson-module-afterburner", version = "2.12.3")
-    implementation(group = "com.fasterxml.jackson.module", name = "jackson-module-kotlin", version = "2.12.3")
-
-    implementation(group = "com.github.kittinunf.fuel", name = "fuel", version = "2.3.1")
-    implementation(group = "com.github.kittinunf.fuel", name = "fuel-jackson", version = "2.3.1")
-
     implementation(group = "be.zvz.alsong", name = "alsong-kt", version = "1.0.6")
-    implementation(group = "com.github.rubenlagus", name = "TelegramBots", version = "5.2.0")
+    implementation(group = "com.github.rubenlagus", name = "TelegramBots", version = "5.6.0")
     implementation(group = "net.sf.javamusictag", name = "jid3lib", version = "0.5.4")
 }
 
